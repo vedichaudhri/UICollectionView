37e92185be44ae22c3ebe09e08510738a890bf21
Vedi Chaudhri <vedichaudhri@gmail.com>
2017-07-12 12:01:12 -0700

    Initial Commit

---
+++ UICollectionViewTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UICollectionViewTutorial
+//
+//  Created by Vedi Chaudhri on 7/12/17.
+//  Copyright © 2017 Vedi Chaudhri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
